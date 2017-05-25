a2f57432e0008687324d8513dc4843079f8ef314
Ryan Kelsey <t-ryke@microsoft.com>
2017-05-24 17:48:14 -0700

    database connection

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var path = require('path');
 var favicon = require('serve-favicon');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var sql = require('mssql');
 
 var app = express();
 
@@ -15,6 +16,28 @@ app.use(express.static(__dirname));
 
 app.get('/', function(req, res) {
 	res.sendFile('index.html');
+});
+
+app.post('/test', function(req, res) {
+	pool = new sql.ConnectionPool({
+		user: 'ryan',
+		password: 'cop4935l!t',
+		server: 'tcp:flockit.database.windows.net',
+		database: 'flockit'
+	});
+
+	pool.connect(function(err) {
+		console.log(err + ' Couldn\'t connect!');
+	});
+
+	request = new sql.Request(pool);
+	request.query('select 1 as number', function(err, result) {
+		console.log(result.recordset[0].number);
+	});
+
+	pool.close();
+
+	res.json({});
 });
 
 var port = process.env.PORT || 1337;
