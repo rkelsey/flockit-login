b8cf8bce719047e36a34d15492292616268f5ff0
Ryan Kelsey <ryan.kelsey01@gmail.com>
2017-05-30 02:24:26 -0400

    Working register page

--- app.js
+++ app.js
@@ -17,6 +17,14 @@ var dbConfig = {
 	}
 }
 
+var connection = new tedious.Connection(dbConfig);
+connection.on('connect', function(err) {
+	if(err) {
+		console.log(err);
+		return;
+	}
+});
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(bodyParser.json());
@@ -29,20 +37,67 @@ app.get('/', function(req, res) {
 });
 
 app.post('/test', function(req, res) {
-	var connection = new tedious.Connection(dbConfig);
+	//var connection = new tedious.Connection(dbConfig);
 	var request = new tedious.Request('SELECT * FROM [dbo].[User]', function(err, rowCount, rows) {
 		console.log(rows);
 		res.json(rows[0][0].value);
 	});
 	
-	connection.on('connect', function(err) {
-		if(err) {
-			console.log(err);
-			res.json({});
+	connection.execSql(request);
+});
+
+app.get('/signup', function(req, res) {
+	res.sendFile(__dirname + '/signup.html');
+});
+
+app.post('/poop', function(req, res) {
+	console.log(JSON.stringify(req.body));
+	var ret = {};
+	ret.error = null;
+	
+	var TYPES = tedious.TYPES;
+	
+	var checkUsername = new tedious.Request('SELECT * FROM [dbo].[User] WHERE username = @user', function(err, rowCount, rows) {
+		if(err)
+			throw err;
+		
+		if(rowCount > 0) {
+			ret.err = 'That username is already taken.';
+			res.json(ret);
+			return;
 		} else {
-			connection.execSql(request);
+			var checkEmail = new tedious.Request('SELECT * FROM [dbo].[User] WHERE email = @email', function(err, rowCount, rows) {
+				if(err)
+					throw err;
+				
+				if(rowCount > 0) {
+					ret.err = 'That email is already being used.';
+					res.json(ret);
+					return;
+				} else {
+					var insert = new tedious.Request('INSERT INTO [dbo].[User] (Username, PasswordHash, Email, FirstName, LastName) VALUES (@user, @pass, @email, @first, @last)', function(err, rows, rowCount) {
+						if(err)
+							throw err;
+						
+						res.json(ret);
+					});
+					
+					insert.addParameter('user', TYPES.VarChar, req.body.username);
+					insert.addParameter('pass', TYPES.VarChar, req.body.password);
+					insert.addParameter('email', TYPES.VarChar, req.body.email);
+					insert.addParameter('first', TYPES.VarChar, req.body.first);
+					insert.addParameter('last', TYPES.VarChar, req.body.last);
+					connection.execSql(insert);
+				}
+			});
+			
+			checkEmail.addParameter('email', TYPES.VarChar, req.body.email);
+			connection.execSql(checkEmail);
 		}
 	});
+	
+	checkUsername.addParameter('user', TYPES.VarChar, req.body.username);
+	connection.execSql(checkUsername);
 });
 
 var port = process.env.PORT || 1337;
